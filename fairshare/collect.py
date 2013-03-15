ae775a717b0e2d22b0f2a1e15f123a236f6e6e21
Jerome Belleman <jerome.belleman@gmail.com>
2013-03-15 11:16:15 +0100

    Clean up old data
    Add logging

--- fairshare/collect.py
+++ fairshare/collect.py
@@ -1,26 +1,39 @@
 #! /usr/bin/env python
 
 import sys
-import pylsf
 from time import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import logging
+
+import pylsf
+import cx_Oracle
 import fairshare
 
 PART = 'SHARE'
 USERDATALEN = 7
 
 def collect(args):
-    connection = fairshare.connect(args.rcfile)
+    # Set up logging
+    logging.basicConfig(filename=args.log, level=logging.INFO,
+                        format="%(asctime)s %(levelname)s %(message)s")
+
+    # Connect to DB
+    try:
+        connection = fairshare.connect(args.config)
+    except cx_Oracle.DatabaseError, e:
+        logging.error(str(e)[:-1])
+        return 1
     c = connection.cursor()
 
+    # Run PyLSF 
     rc = pylsf.lsb_init()
     if rc != 0:
         raise Exception(pylsf.lsb_sysmsg())
 
     t0 = time()
     (_, _, userdata), = pylsf.lsb_hostpartinfo([PART])
-    print "Queried from LSF in %.2f s" % (time() - t0)
+    logging.info("Queried from LSF in %.2f s" % (time() - t0))
 
     users = []
     try:
@@ -41,13 +54,14 @@ def collect(args):
         # need more than 0 values to unpack
         pass
 
-    # DB statement
+    # Insert new share values
+    # http://guyharrison.squarespace.com/blog/2010/1/1/the-11gr2-ignore_row_on_dupkey_index-hint.html
+    # Reuse nextval: http://dba.stackexchange.com/questions/2978
     insert = '''\
     INSERT INTO shares (id, timestamp, shares, priority, started, cpu, wall)
     VALUES (id, :t, :shares, :priority, :started, :cpu, :wall);'''
 
-    # http://guyharrison.squarespace.com/blog/2010/1/1/the-11gr2-ignore_row_on_dupkey_index-hint.html
-    # Reuse nextval: http://dba.stackexchange.com/questions/2978
+
     sql = '''\
 DECLARE
     id NUMBER(7);
@@ -65,20 +79,34 @@ END;''' % ((insert,) * 2)
 
     t0 = time()
     c.executemany(sql, users)
+    logging.info("Inserted %d records to DB in %.2f s" % \
+        (c.rowcount, time() - t0))
+
+    # Clean up old data
+    sql = 'DELETE FROM shares WHERE timestamp < :t'
+    t0 = time()
+    c.execute(sql, [datetime.now() - timedelta(10)])
+    logging.info("Deleted %d records from DB in %.2f s" % \
+        (c.rowcount, time() - t0))
+
+    # Commit
+    t0 = time()
     connection.commit() # Yes, needed
-    print "Inserted to DB in %.2f s" % (time() - t0)
+    logging.info("Committed changes in DB in %.2f s" % (time() - t0))
 
 def main():
     p = ArgumentParser(description="Modify fairshare DB",
                        formatter_class=ArgumentDefaultsHelpFormatter)
-    p.add_argument('-f', '--file', metavar='FILE', dest='rcfile',
-                   help="config file", default='/etc/fairshare')
+    p.add_argument('-c', '--config', help="config file",
+                   default='/etc/fairshare')
+    p.add_argument('-l', '--log', help="log file",
+                   default='/var/log/fairshare.log')
     s = p.add_subparsers()
     pmk = s.add_parser('collect', help="Collect users and their stats")
     pmk.set_defaults(func=collect)
     args = p.parse_args()
 
-    args.func(args)
+    return args.func(args)
 
 if __name__ == '__main__':
     sys.exit(main())
