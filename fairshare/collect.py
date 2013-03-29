9e04a12a44a22d8247f442da792878a75abf6c44
Jerome Belleman <jerome.belleman@gmail.com>
2013-03-29 16:10:23 +0100

    Improve error handling and remove inserted row count

--- fairshare/collect.py
+++ fairshare/collect.py
@@ -32,50 +32,51 @@ def collect(username, password, dsn, log, delete=DEL):
     sys.stdout = Output(logging.warning)
     sys.stderr = Output(logging.error)
 
-    # Connect to DB
-    connection = connect(username,
-                         open(expanduser(password)).read().strip(),
-                         dsn)
-    c = connection.cursor()
-
-    # Run PyLSF 
-    rc = pylsf.lsb_init()
-    if rc != 0:
-        logging.error(pylsf.lsb_sysmsg())
-        return 1
-
-    t0 = time()
-    (_, _, userdata), = pylsf.lsb_hostpartinfo([PART])
-    logging.info("Retrieved %d groups from LSF in %.2f s" % \
-                 (len(userdata) / 7, time() - t0))
-
-    t = datetime.now()
-    users = []
     try:
-        i = 0
-        while True:
-            # Not using reserved and adjust is missing
-            user, shares, priority, started, cpu, reserved, wall = \
-                userdata[USERDATALEN * i + 0:USERDATALEN * i + 7]
+        # Connect to DB
+        connection = connect(username,
+                             open(expanduser(password)).read().strip(),
+                             dsn)
+        c = connection.cursor()
 
-            # Collect data
-            d = {'u': user, 't': t, 'shares': shares, 'priority': priority,
-                 'started': started, 'cpu': cpu, 'wall': wall}
-            users.append(d)
+        # Run PyLSF 
+        rc = pylsf.lsb_init()
+        if rc != 0:
+            logging.error(pylsf.lsb_sysmsg())
+            return 1
 
-            i += 1
-    except ValueError:
-        # need more than 0 values to unpack
-        pass
+        t0 = time()
+        (_, _, userdata), = pylsf.lsb_hostpartinfo([PART])
+        logging.info("Retrieved %d groups from LSF in %.2f s" % \
+                     (len(userdata) / 7, time() - t0))
 
-    # Insert new share values
-    insert = '''\
-    INSERT INTO shares (id, timestamp, shares, priority, started, cpu, wall)
-    VALUES (id, :t, :shares, :priority, :started, :cpu, :wall);'''
+        t = datetime.now()
+        users = []
+        try:
+            i = 0
+            while True:
+                # Not using reserved and adjust is missing
+                user, shares, priority, started, cpu, reserved, wall = \
+                    userdata[USERDATALEN * i + 0:USERDATALEN * i + 7]
 
-    # http://guyharrison.squarespace.com/blog/2010/1/1/the-11gr2-ignore_row_on_dupkey_index-hint.html
-    # Reuse nextval: http://dba.stackexchange.com/questions/2978
-    sql = '''\
+                # Collect data
+                d = {'u': user, 't': t, 'shares': shares, 'priority': priority,
+                     'started': started, 'cpu': cpu, 'wall': wall}
+                users.append(d)
+
+                i += 1
+        except ValueError:
+            # need more than 0 values to unpack
+            pass
+
+        # Insert new share values
+        insert = '''\
+        INSERT INTO shares (id, timestamp, shares, priority, started, cpu, wall)
+        VALUES (id, :t, :shares, :priority, :started, :cpu, :wall);'''
+
+        # http://guyharrison.squarespace.com/blog/2010/1/1/the-11gr2-ignore_row_on_dupkey_index-hint.html
+        # Reuse nextval: http://dba.stackexchange.com/questions/2978
+        sql = '''\
 DECLARE
     id NUMBER(7);
 BEGIN
@@ -90,22 +91,24 @@ EXCEPTION WHEN no_data_found THEN
 %s
 END;''' % ((insert,) * 2)
 
-    t0 = time()
-    c.executemany(sql, users)
-    logging.info("Inserted %d records to DB in %.2f s" % \
-                 (c.rowcount, time() - t0))
+        t0 = time()
+        c.executemany(sql, users)
+        logging.info("Inserted records to DB in %.2f s" % (time() - t0))
 
-    # Clean up old data
-    sql = 'DELETE FROM shares WHERE timestamp < :t'
-    t0 = time()
-    c.execute(sql, [datetime.now() - timedelta(delete)])
-    logging.info("Deleted %d records from DB in %.2f s" % \
-                 (c.rowcount, time() - t0))
+        # Clean up old data
+        sql = 'DELETE FROM shares WHERE timestamp < :t'
+        t0 = time()
+        c.execute(sql, [datetime.now() - timedelta(delete)])
+        logging.info("Deleted %d records from DB in %.2f s" % \
+                     (c.rowcount, time() - t0))
 
-    # Commit
-    t0 = time()
-    connection.commit() # Yes, needed
-    logging.info("Committed changes to DB in %.2f s" % (time() - t0))
+        # Commit
+        t0 = time()
+        connection.commit() # Yes, needed
+        logging.info("Committed changes to DB in %.2f s" % (time() - t0))
+    except (IOError, DatabaseError), e:
+        logging.error(str(e).strip())
+        return 1
 
 def main():
     p = ArgumentParser(description="Modify fairshare DB",
@@ -120,12 +123,9 @@ def main():
                    help="delete data older than N days", type=int, default=DEL)
     args = p.parse_args()
 
-    try:
-        # Run command
-        collect(args.username, args.password, args.dsn, args.log, args.delete)
-    except (IOError, DatabaseError), e:
-        logging.error(str(e).strip())
-        return 1
+    # Run command
+    return collect(args.username, args.password, args.dsn,
+                   args.log, args.delete)
 
 if __name__ == '__main__':
     sys.exit(main())
